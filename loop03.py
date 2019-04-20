a2a9fc40cf618e006c910f16d4d4a20a92362472
wnrkdms <49799900+wnrkdms@users.noreply.github.com>
2019-04-20 11:09:04 +0900

    Add files via upload

---
+++ loop03.py
@@ -0,0 +1,31 @@
+#range 함수 연습하기
+
+print("1부터 10까지")
+for i in range (1,10,1):
+    print(i,end=" ")
+print()
+
+print("0부터 9까지")
+for i in range(10):
+    print(i,end=" ")
+print()
+
+print("0부터 10까지")
+for i in range(11):
+    print(i,end=" ")
+print()
+
+print("1부터 10까지")
+for i in range(1,11):
+    print(i,end=" ")
+print()
+
+print("1부터 10까지의 수 중에 홀수")
+for i in range(1,11,2):
+    print(i,end=" ")
+print()
+
+print("1부터 10까지의  수 중에 짝수")
+for i in range(2,11,2):
+    print(i,end=" ")
+print()
